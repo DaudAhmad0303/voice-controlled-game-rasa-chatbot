40f99abd42b16af7450075dd3c8168b3d8890c44
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2022-08-29 13:23:37 +0500

    started working on slots
    story completed upto A1.1

--- actions/actions.py
+++ actions/actions.py
@@ -12,6 +12,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from random import randint
+from rasa_sdk.events import SlotSet
 
 class ActionThrowDice(Action):
 
@@ -22,11 +23,16 @@ class ActionThrowDice(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+#         res = '''You have killed all the goblins and arrived at a secret tunnel that leads
+# to necromancer's black tower. However, there is a lock on the door. From one to six
+# guess the number of a single dice throw. What is your guess?'''
         number = randint(1,6)
         if number < 3:
-            dispatcher.utter_message(text=str(number))
+            # dispatcher.utter_message(text=str(number) + "You were hit by the goblins' arrows and died")
+            return [SlotSet("dice_throw_1", True)]
         else:
-            pass
+            # dispatcher.utter_message(text=res)
+            return []
             
 
-        return []
+        # return []
