ba772e69f401091fc075100b9eff0232098279d0
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2022-08-30 14:30:21 +0500

    First suceess story running
    First sucess story is perfectly running, setting slot is also fixed, two killed stories are also given

--- actions/actions.py
+++ actions/actions.py
@@ -14,25 +14,82 @@ from rasa_sdk.executor import CollectingDispatcher
 from random import randint
 from rasa_sdk.events import SlotSet
 
-class ActionThrowDice(Action):
+class ActionThrowDice1(Action):
+    '''Throw a dice and sets the slot``dice_throw_1`` 
+    if number is greater than or equal to `3`, does nothing otherwise.'''
 
     def name(self) -> Text:
-        return "action_throw_dice"
+        return "action_throw_dice_1"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-#         res = '''You have killed all the goblins and arrived at a secret tunnel that leads
-# to necromancer's black tower. However, there is a lock on the door. From one to six
-# guess the number of a single dice throw. What is your guess?'''
         number = randint(1,6)
-        if number < 3:
-            # dispatcher.utter_message(text=str(number) + "You were hit by the goblins' arrows and died")
+        if number >= 3:
             return [SlotSet("dice_throw_1", True)]
         else:
-            # dispatcher.utter_message(text=res)
             return []
-            
 
-        # return []
+
+class ActionThrowDice2(Action):
+    '''Throw a dice and sets the slot``dice_throw_2`` 
+    if number is greater than or equal to `3`, does nothing otherwise.'''
+    
+    def name(self) -> Text:
+        return "action_throw_dice_2"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        number = randint(1,6)
+        if number >= 3:
+            return [SlotSet("dice_throw_2", True)]
+        else:
+            return []
+
+class ActionThrowDice3(Action):
+    '''Throw a dice and sets the slot``dice_throw_3`` 
+    if number is greater than or equal to `3`, does nothing otherwise.'''
+    
+    def name(self) -> Text:
+        return "action_throw_dice_3"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        number = randint(1,6)
+        if number >= 3:
+            return [SlotSet("dice_throw_3", True)]
+        else:
+            return []
+
+class ActioncCheckTunnelGuess1(Action):
+    '''Checking the Guess entered by user. If it is in range
+    ``range(1,6]``, the guess is correct, uncorrect otherwise.'''
+    
+    def name(self) -> Text:
+        return "action_check_tunnel_lock_guess"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Return the None value if the entity value is not set.
+        g = next(tracker.get_latest_entity_values("guess_1"), None)
+        
+        if g is None:
+            dispatcher.utter_message(text='What is your guess?')
+        g = int(g)
+        
+        prev_slot = tracker.get_slot("dice_throw_1")
+        print(prev_slot)
+        if g in list(range(1,7)) and prev_slot == True:
+            return [SlotSet("correct_guess_for_tunnel", True)]
+        else:
+            return [SlotSet("correct_guess_for_tunnel", False)]
+
+
+
